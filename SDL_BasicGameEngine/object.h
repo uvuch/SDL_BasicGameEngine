adad0ac6b4d9552d49108da7ba9c6d4a522ba228
Serge Muzyka <siarhei.muzyka@pennmedicine.upenn.edu>
2023-08-17 19:52:53 -0400

    Added custom event handlers to Object and successors.

--- SDL_BasicGameEngine/object.h
+++ SDL_BasicGameEngine/object.h
@@ -9,12 +9,18 @@
 #define object_h
 
 #include <SDL2/SDL.h>
+#include <map>
+
+typedef bool (*eventHandler)(const SDL_Event&);
 
 class Object {
 public:
-    virtual bool handleEvents(const SDL_Event& event) = 0;
+    bool handleEvents(const SDL_Event& event);
     virtual void update() = 0;
     virtual void draw() = 0;
+    
+protected:
+    std::map<Uint32, eventHandler> m_eventHandlers;
 };
 
 #endif /* object_h */
